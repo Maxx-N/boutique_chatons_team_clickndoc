e606f9c825c2773690d62a72d7db4c7466d6f37c
Christophe BONNEFOY <christophe48bonnefoy@gmail.com>
2019-11-25 14:52:05 +0100

    Seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,12 +22,13 @@ puts "User : #{Faker::Name.first_name}, cart create"
 c << Cart.create(user_id: u[index].id)
 end
 
+def round(number,precision=2)
+  ("%01.#{precision}f" %number).to_f
+end
 40.times do
-i << Item.create(title: Faker::Book.title, description: Faker::Lorem.sentence(word_count: 20), price: rand(1.00..100.99), image_url: "https://static.wamiz.fr/images/articles/facebook/article/chaton-jouets-fb-59a3ea83c1a7d.jpg")
+i << Item.create(title: Faker::Book.title, description: Faker::Lorem.sentence(word_count: 20), price: round(rand(1.00..100.99)), image_url: "https://static.wamiz.fr/images/articles/facebook/article/chaton-jouets-fb-59a3ea83c1a7d.jpg")
 puts "Item: #{Faker::Book.title}"
 end
-
-
 
 20.times do
   ItemCart.create(cart_id: c.sample, item_id: i.sample)
