976b1cc45f3a86ccc8c61b6e29289e90d189ff37
Christophe BONNEFOY <christophe48bonnefoy@gmail.com>
2019-11-25 12:01:55 +0100

    Seed + devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_102555) do
+ActiveRecord::Schema.define(version: 2019_11_25_103736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,10 +40,17 @@ ActiveRecord::Schema.define(version: 2019_11_25_102555) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "nom"
-    t.string "prenom"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+Cart.destroy_all
+Item.destroy_all
+Order.destroy_all
+u = [] #tableau User
+i = [] #Tableau Item
+c = [] #tableau Cart
+
+5.times do
+u << User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Name.first_name + "@yopmail.com", password: "fauxuser")
+puts "User : #{Faker::Name.first_name}"
+end
+
+40.times do
+i << Item.create(title: Faker::Book.title, description: Faker::Lorem.sentence(word_count: 20), price: rand(1..100)+","+rand(1..9)+rand(1..9), image: "https://static.wamiz.fr/images/articles/facebook/article/chaton-jouets-fb-59a3ea83c1a7d.jpg")
+puts "Item: #{Faker::Book.title}"
+end
+
+5.times do
+c << Cart.create(user_id: u.sample)
+end
+puts "Cart create"
+
+20.times do
+  ItemCart.create(cart_id: c.sample, item_id: i.sample)
+end
+puts "ItemCart create"
+
+clength = c.length
+clength.times do |index|
+Order.create(cart_id: c[index])
+end
+puts "Order Create"
