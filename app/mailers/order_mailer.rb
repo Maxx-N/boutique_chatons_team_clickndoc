04d475640a8e385b7c132dafd191819222c1c760
LucasJego <jego.lucas@gmail.com>
2019-11-26 18:00:57 +0100

    add .gitignore + mail to confirm order

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,8 +2,17 @@ class OrderMailer < ApplicationMailer
 
 	def confirm_order_email(order)
 		@order = order
+
+
+    @items_array = Item.where(ItemOrder.where(order_id: @order.id))
+    # ItemOrder.where(order_id: @order.id) -> on cherche dans la table ItemOrder les éléments dont l'id vaut @order.id
+    # puis on regarde la colonne "item_id" de cet élément
+
+
+
 		@url = 'http://boutique_chatons_team_clickndoc.fr/login'
 		mail(to: @order.cart.user.email, subject: 'Confirmation de votre commande.')
 	end
 
 end
+ 
