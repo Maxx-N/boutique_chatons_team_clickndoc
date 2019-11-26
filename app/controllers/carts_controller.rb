7472e8b3771125fc9798930e909f9a890ad85480
Maxx-N <maximenorval@gmail.com>
2019-11-26 10:17:41 +0100

    link to add to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,8 +11,6 @@ class CartsController < ApplicationController
     ic.cart_id = current_user.id
     ic.item_id = Item.find(params[:id]).id
     ic.save
-    puts ic.errors
-    tp current_user.cart.items, :title
   end
 
   def destroy
