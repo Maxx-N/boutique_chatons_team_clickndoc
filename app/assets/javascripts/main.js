351c5f7a8c2362e5cd8e313b3a33bfc466a1a083
ClaireHien <clairehien.osmont@gmail.com>
2019-11-28 15:48:59 +0100

    bar

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,28 +7,28 @@ function dropdownNavbar() {
   let menuNavbar = document.getElementsByClassName("menu-navbar");
   let statusNavbar = [];
 
-  for (let n = 0; n < btnNavbar.length; n++) {
-    var st = statusNavbar.push(false);
+    for (let n = 0; n < btnNavbar.length; n++) {
+      var st = statusNavbar.push(false);
 
-    function drop() {
-      if (statusNavbar[n] == false) {
-        for (let i = 0; i < btnNavbar.length; i++) {
-          if ( i !== n) {
-              menuNavbar[i].style.top = "";
-              statusNavbar[i] = false;
+      function drop() {
+        if (statusNavbar[n] == false) {
+          for (let i = 0; i < btnNavbar.length; i++) {
+            if ( i !== n) {
+                menuNavbar[i].style.top = "";
+                statusNavbar[i] = false;
+            };
           };
+          menuNavbar[n].style.top = "50px";
+          statusNavbar[n] = true;
+        } else if (statusNavbar[n] == true) {
+          menuNavbar[n].style.top = "";
+          statusNavbar[n] = false;
         };
-        menuNavbar[n].style.top = "50px";
-        statusNavbar[n] = true;
-      } else if (statusNavbar[n] == true) {
-        menuNavbar[n].style.top = "";
-        statusNavbar[n] = false;
       };
+
+    btnNavbar[n].addEventListener("click", drop);
+
     };
-
-  btnNavbar[n].addEventListener("mouseover", drop);
-
-  };
 
 };
 
