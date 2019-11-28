b15a6cb000c48b1b2e231aae8d5910603392a8fa
Maxx-N <maximenorval@gmail.com>
2019-11-28 13:32:47 +0100

    mouseover navbar

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -26,7 +26,7 @@ function dropdownNavbar() {
       };
     };
 
-  btnNavbar[n].addEventListener("click", drop);
+  btnNavbar[n].addEventListener("mouseover", drop);
 
   };
 
@@ -49,6 +49,6 @@ function dropdownSearchByTag() {
     };
   };
 
-  btnTag[0].addEventListener("click", drop);
+  btnTag[0].addEventListener("mouseover", drop);
 
 };
